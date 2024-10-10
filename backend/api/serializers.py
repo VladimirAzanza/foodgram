5ab5b1cf7dc6f2af7eb59bec5140b788a0e97135
Vladimir Azanza <vladimirazanza@gmail.com>
2024-10-10 17:52:09 +0300

    Add required fields to user serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -20,5 +20,19 @@ class CustomUserSerializer(serializers.ModelSerializer):
         )
         read_only_fields = ('avatar', 'is_subscribed')
         extra_kwargs = {
-            'password': {'write_only': True}
+            'password': {
+                'write_only': True, 'required': True, 'allow_blank': False
+            },
+            'email': {
+                'required': True, 'allow_blank': False
+            },
+            'username': {
+                'required': True, 'allow_blank': False
+            },
+            'first_name': {
+                'required': True, 'allow_blank': False
+            },
+            'last_name': {
+                'required': True, 'allow_blank': False
+            },
         }
