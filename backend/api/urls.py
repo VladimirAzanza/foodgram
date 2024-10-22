744b0b8d4db4a55c3806817a44bf4388ec29fad6
Vladimir Azanza <vladimirazanza@gmail.com>
2024-10-22 07:05:07 +0300

    Delete unnecesary things

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -18,18 +18,10 @@ router_v1.register('recipes', RecipeViewSet, basename='recipe')
 router_v1.register('ingredients', IngredientViewSet, basename='ingredient')
 
 urlpatterns = [
-    # path('users/me/', CustomCurrentUser.as_view({
-    #    'get': 'me',
-    #    'put': 'me',
-    #    'patch': 'me',
-    #    'delete': 'me'
-    # })),
-    # path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('users/me/avatar/', CurrentUserAvatar.as_view({
         'put': 'update',
         'delete': 'destroy'
     })),
-    # path('users/subscriptions/', ),
     path('', include(router_v1.urls))
 ]
