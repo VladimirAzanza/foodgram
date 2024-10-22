60f9f2edf63b2744c9790fc16f1b88a65ce62cf0
Vladimir Azanza <vladimirazanza@gmail.com>
2024-10-22 18:42:32 +0300

    Add cors headers to check frontend+backend

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'django_filters',
     'rest_framework.authtoken',
     'djoser',
@@ -64,6 +65,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -163,3 +165,8 @@ DJOSER = {
     },
     'USER_ID_FIELD': 'pk'
 }
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
