58e3f09fd7464875418c4d9b84c035a37c336bc8
Vladimir Azanza <vladimirazanza@gmail.com>
2024-10-25 10:35:39 +0300

    Add unique constraint to ingredient and measurement unit

--- backend/ingredients/models.py
+++ backend/ingredients/models.py
@@ -17,6 +17,12 @@ class Ingredient(models.Model):
     class Meta:
         verbose_name = "Ингредиент"
         verbose_name_plural = "Ингредиенты"
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'measurement_unit'],
+                name='unique_ingredient_measurement_unit'
+            )
+        ]
 
     def __str__(self):
         return self.name
