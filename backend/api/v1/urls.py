68a66d88575380ac8736db3e9b45d80e05583035
Vladimir Azanza <vladimirazanza@gmail.com>
2024-11-05 15:43:18 +0300

    Small fix at urls

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -3,19 +3,24 @@ from django.urls import include, path
 app_name = 'api_v1'
 
 urlpatterns = [
-    path('auth/', include(
-        'djoser.urls.authtoken'
-    )),
-    path('users/', include(
-        'api.v1.users.urls', namespace='users'
-    )),
-    path('tags/', include(
-        'api.v1.tags.urls', namespace='tags'
-    )),
-    path('recipes/', include(
-        'api.v1.recipes.urls', namespace='recipes'
-    )),
-    path('ingredients/', include(
-        'api.v1.ingredients.urls', namespace='ingredients'
-    )),
+    path(
+        'auth/',
+        include('djoser.urls.authtoken')
+    ),
+    path(
+        'users/',
+        include('api.v1.users.urls', namespace='users')
+    ),
+    path(
+        'tags/',
+        include('api.v1.tags.urls', namespace='tags')
+    ),
+    path(
+        'recipes/',
+        include('api.v1.recipes.urls', namespace='recipes')
+    ),
+    path(
+        'ingredients/',
+        include('api.v1.ingredients.urls', namespace='ingredients')
+    ),
 ]
