a7c6da0fb12aba8959b6c72201e3f6e1e35b9712
Vladimir Azanza <vladimirazanza@gmail.com>
2024-11-05 16:22:31 +0300

    Add two comments

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
+# from django.core.exceptions import ValidationError
 from django.db import models
 
 from ingredients.models import Ingredient
 from tags.models import Tag
 
 from .constants import (
-    AT_LEAST_ONE_INGREDIENT_MESSAGE,
+    # AT_LEAST_ONE_INGREDIENT_MESSAGE,
     DEFAULT_COOKING_TIME,
     MAX_LENGTH_NAME_FIELD
 )
