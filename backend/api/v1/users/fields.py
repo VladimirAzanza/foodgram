5556cf923bc912dd29e441111fb06e87848c3123
Vladimir Azanza <vladimirazanza@gmail.com>
2024-11-05 16:02:01 +0300

    Fix small thing related to PEP8 and refactored get_boolean if user is subscribed

--- backend/api/v1/users/fields.py
+++ backend/api/v1/users/fields.py
@@ -2,11 +2,8 @@ from users.models import Subscription
 
 
 def get_boolean_if_user_is_subscribed(user, following):
-    if user.is_authenticated:
-        if Subscription.objects.filter(
+    return (
+        user.is_authenticated and Subscription.objects.filter(
             user=user, following=following
-        ):
-            return True
-        else:
-            return False
-    return False
+        ).exists()
+    )
--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.db.models import Count
 
 from recipes.models import Favorite, IngredientRecipe, Recipe, ShoppingCart
 
